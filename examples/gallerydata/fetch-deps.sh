122ea4d9040c2ac9f1abaead145442877c0db296
JBBgameich <jbbgameich@gmail.com>
2018-05-20 23:16:17 +0200

    Disable including QQC2 in click

--- examples/gallerydata/fetch-deps.sh
+++ examples/gallerydata/fetch-deps.sh
@@ -6,7 +6,7 @@ DEB_HOST_MULTIARCH=$(dpkg-architecture -qDEB_HOST_MULTIARCH)
 DEB_TARGET_DIR="$SOURCES/build/deps"
 
 KIRIGAMI_VERSION="5.44.0-2"
-QQC2_VERSION="5.9.5-0ubports2"
+#QQC2_VERSION="5.9.3-0ubports3"
 
 mkdir $DEB_TARGET_DIR -p
 
@@ -28,10 +28,10 @@ download_deps() {
 		install_deb http://snapshot.debian.org/archive/debian/20180430T215634Z/pool/main/k/kirigami2 ${PKG} ${KIRIGAMI_VERSION}
 	done
 
-	echo "I: Installing QtQuick Controls 2"
-	for PKG in qml-module-qtquick-controls2 libqt5quickcontrols2-5 qtquickcontrols2-5-dev qml-module-qtquick-templates2 qml-module-qt-labs-platform libqt5quicktemplates2-5 libqt5quicktemplates2-5; do
-		install_deb https://repo.ubports.com/pool/xenial/main/q/qtquickcontrols2-opensource-src ${PKG} ${QQC2_VERSION}
-	done
+#	echo "I: Installing QtQuick Controls 2"
+#	for PKG in qml-module-qtquick-controls2 libqt5quickcontrols2-5 qtquickcontrols2-5-dev qml-module-qtquick-templates2 qml-module-qt-labs-platform libqt5quicktemplates2-5 libqt5quicktemplates2-5; do
+#		install_deb https://repo.ubports.com/pool/xenial/main/q/qtquickcontrols2-opensource-src ${PKG} ${QQC2_VERSION}
+#	done
 
 
 	echo "I: Installing QML modules"
